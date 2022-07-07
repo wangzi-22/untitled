5240f1a76b9364a44afac896cf12a690238f7940
tiancai <amam@amam>
2022-07-07 13:45:24 +0800

    three commit

--- src/main/java/com/chunyu/hh.java
+++ src/main/java/com/chunyu/hh.java
@@ -4,5 +4,6 @@ public class hh {
     public static void main(String[] args) {
         System.out.println("nihaoya");
         System.out.println("我不好");
+        System.out.println("我最好");
     }
 }
